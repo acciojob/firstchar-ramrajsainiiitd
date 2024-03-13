c18e25d6d4ea87995227db2de9134d1c941d02a4
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-13 10:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function firstChar(text) {
   // your code here
+	str = text.trim();
+  if(str.length == 0){
+    return "";
+  }
+  else{
+    return str[0];
+  }
 }
-
 // Do not change the code below
 
 const text = prompt("Enter text:");
